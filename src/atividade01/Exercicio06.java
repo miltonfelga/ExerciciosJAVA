3d9f7cbecbc6f4725ea724bce53885217cdf7948
milton felga <milton.felga@etec.sp.gov.br>
2019-09-24 22:21:11 -0300

    colocar catch cancelar

--- src/atividade01/Exercicio06.java
+++ src/atividade01/Exercicio06.java
@@ -7,19 +7,28 @@ public class Exercicio06 {
     public static void main(String[] args) {
         int dd,mm,aaaa;
         int biss;
-        int ddh=19,mmh=9,aaaah=2019;
+        int ddh=0,mmh=0,aaaah=0;
         int mes,dias=0,somad=0,somam=0,somaa=0,somadias;
         String aux=null,st=null;
         
         try{
         
-        aux = JOptionPane.showInputDialog(null, "Digite o Dia ");
+        aux = JOptionPane.showInputDialog(null, "Digite o Dia atual");
+        ddh = Integer.parseInt(aux);
+        
+        aux = JOptionPane.showInputDialog(null, "Digite o Mes atual");
+        mmh = Integer.parseInt(aux);
+        
+        aux = JOptionPane.showInputDialog(null, "Digite o Ano atual ");
+        aaaah = Integer.parseInt(aux);    
+        
+        aux = JOptionPane.showInputDialog(null, "Digite o Dia em que você nasceu");
         dd = Integer.parseInt(aux);
         
-        aux = JOptionPane.showInputDialog(null, "Digite o Mes ");
+        aux = JOptionPane.showInputDialog(null, "Digite o Mes em que você nasceu");
         mm = Integer.parseInt(aux);
         
-        aux = JOptionPane.showInputDialog(null, "Digite o Ano ");
+        aux = JOptionPane.showInputDialog(null, "Digite o Ano em que você nasceu");
         aaaa = Integer.parseInt(aux);
 
         if(dd>ddh){
@@ -34,12 +43,16 @@ public class Exercicio06 {
             somam-=1;
         }
         
-        
+        if(aaaa<aaaah){
         aaaa=aaaah-aaaa;
         biss=aaaa/4;
         somaa=(aaaa*365)+biss;
-        
-
+        }
+        else{
+             JOptionPane.showMessageDialog(null,"Coloque uma data anterior a atual");
+             main(args); 
+             
+        }
         for(mes=mm;mes!=mmh;mes++){
             if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
                 dias=31;
@@ -78,7 +91,7 @@ public class Exercicio06 {
     }
            catch (NumberFormatException erro) {
            JOptionPane.showMessageDialog(null, "Houve erro na conversão, digite apenas caracteres numericos");
-           main(args); 
+            
         
         }
     }
